a0ae92b27bf1c807b836357a17e14aaaad48a8c2
Mike <lumen.net@yandex.ru>
2023-05-23 14:42:29 +0400

    Refactoring the talk module

--- src/modules/talk.js
+++ src/modules/talk.js
@@ -1,59 +1,22 @@
-import Datastore from 'nedb';
-import { getTimeBeforeEvent } from '../common/getTimeBeforeEvent';
-
-
-/*
-*   Реакция на фразу: "Ты че пес" и ее производные.
-*/
-const WHAT_ARE_YOU_DOG =
-    ['ты че пес', 'ты че пёс', 'ты че, пес', 'ты че, пес', 'ты пес', 'ты, пес', 'ты пёс', 'ты, пёс', 'пёс', 'пес', 'псина'];
-
-export const whatAreYouDog = (ctx) => {
-    ctx.reply('Гав!');
-};
-
-
-/*
-*   Реакция на фразу: "Ну че там" и ее производные.
-*/
+/**
+ *  The reaction to the phrase: "what's up" and its derivatives.
+ */
 const WHATS_UP_THERE =
-    ['ну че там', 'ну чё там', 'ну чо там'];
+    ["what's up", "what's up!", "whats up", "whats up!", "whatsup", "whatsup!",];
 
 export const whatsUpThere = (ctx) => {
-    var db = new Datastore({ filename: './src/db/event', autoload: true });
-    db.loadDatabase();
-
-    db.find({ key: 'eventTimestamp' }, function (err, eventTimestamp) {
-        if (!eventTimestamp)
-            ctx.replyWithHTML(`Ошибка`);
-        else {
-            let timerText = getTimeBeforeEvent(eventTimestamp[0].value);
-            if (timerText === false)
-                ctx.replyWithHTML(`<b>А все</b>`);
-            else
-                ctx.replyWithHTML(`Ещё <b>${timerText}</b> до бухыча`);
-        }
-    });
+    ctx.reply("Wassup!");
 };
 
 
-/*
-*   The main function of calling dialogs.
-*/
+/**
+ *  The main function of calling dialogs.
+ */
 export const getTalk = (ctx) => {
     let message = ctx.update.message.text.toLowerCase();
 
-    // Реакция на фразу: "Ты че пес" и ее производные.
-    if (WHAT_ARE_YOU_DOG.includes(message)) {
-        whatAreYouDog(ctx);
-    };
-
-    // Реакция на фразу: "Ну че там" и ее производные.
+    // The reaction to the phrase: "what's up" and its derivatives.
     if (WHATS_UP_THERE.includes(message)) {
         whatsUpThere(ctx);
     };
 };
-
-
-
-
