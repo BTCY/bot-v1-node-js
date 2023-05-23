e0997fa68756cd56b82cecabcabe5e5f18deab2f
Mike <lumen.net@yandex.ru>
2023-05-23 14:59:20 +0400

    Edits in events

--- src/modules/event.js
+++ src/modules/event.js
@@ -1,44 +1,43 @@
-import Datastore from 'nedb';
-import { getTimeBeforeEvent } from '../common/getTimeBeforeEvent';
+import Datastore from "nedb";
+import { getTimeBeforeEvent } from "../common/getTimeBeforeEvent";
 
 
-/*
-*   Get how much time is left before the set event.
-*/
+/**
+ *  Get how much time is left before the set event.
+ */
 export const getEvent = (ctx) => {
-    var db = new Datastore({ filename: './src/db/event', autoload: true });
+    var db = new Datastore({ filename: "./src/db/event", autoload: true });
     db.loadDatabase();
 
-    db.find({ key: 'eventTimestamp' }, function (err, eventTimestamp) {
+    db.find({ key: "eventTimestamp" }, (_, eventTimestamp) => {
         if (!eventTimestamp)
-            ctx.replyWithHTML(`Ошибка`);
+            ctx.replyWithHTML("Error!");
         else {
             let timerText = getTimeBeforeEvent(eventTimestamp[0].value);
             if (timerText === false)
-                ctx.replyWithHTML(`<b>Событие прошло!</b>`);
+                ctx.replyWithHTML("<b>The event has passed!</b>");
             else
-                ctx.replyWithHTML(`До события осталось <b>${timerText}</b>`);
+                ctx.replyWithHTML(`<b>${timerText}</b> left before the event.`);
         };
     });
 };
 
 
-/*
-*    Set the date of the event.
-*/
+/**
+ *  Set the date of the event.
+ */
 export const setEvent = (ctx) => {
     var dateReg = /^\d{2}([.])\d{2}\1\d{4}([ ])\d{2}([:])\d{2}$/;
-    let [first, ...rest] = ctx.update.message.text.split(' ');
-    let newEventTimestamp = rest.join(' ') || undefined;
+    let [first, ...rest] = ctx.update.message.text.split(" ");
+    let newEventTimestamp = rest.join(" ").trim() || undefined;
     if (!!newEventTimestamp && dateReg.test(newEventTimestamp)) {
-        var db = new Datastore({ filename: './src/db/event', autoload: true });
+        var db = new Datastore({ filename: "./src/db/event", autoload: true });
         db.loadDatabase();
-        ctx.replyWithHTML(`OK`);
-        // db.insert({ key: 'eventTimestamp', value: newEventTimestamp });
-        db.update({ key: 'eventTimestamp' }, { key: 'eventTimestamp', value: newEventTimestamp }, {});
+        ctx.replyWithHTML("OK");
+        db.update({ key: "eventTimestamp" }, { key: "eventTimestamp", value: newEventTimestamp }, {});
     }
     else {
-        ctx.replyWithHTML(`<b>ОШИБКА</b>\nНеправильный формат даты / времени. Правильный формат - дд.мм.ггг чч.мм (прим. 31.12.2024 21:00)`);
+        ctx.replyWithHTML("<b>ERROR</b>\nWrong date/time format. The correct format is dd.mm.yy hh.mm (example /set_event 31.12.2024 21:00)");
     };
 };
 
