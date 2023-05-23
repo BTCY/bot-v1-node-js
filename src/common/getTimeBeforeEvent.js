ddfac0f7893989bfd55454cb18be55deeadc5d56
Mike <lumen.net@yandex.ru>
2023-05-23 15:01:34 +0400

    Changes in getTimeBeforeEvent file

--- src/common/getTimeBeforeEvent.js
+++ src/common/getTimeBeforeEvent.js
@@ -1,14 +1,14 @@
-import * as dateFns from 'date-fns';
-import ru from 'date-fns/locale/ru';
+import * as dateFns from "date-fns";
+import ru from "date-fns/locale/ru";
 
 
-/*
-*   Get how much is left before the event.
-*/
+/**
+ *  Get how much is left before the event.
+ */
 export const getTimeBeforeEvent = (eventDateTime) => {
     let currentTimestamp = new Date().getTime();
-    let eventDate = eventDateTime.split(' ')[0].split('.');
-    let eventTime = eventDateTime.split(' ')[1].split(':');
+    let eventDate = eventDateTime.split(" ")[0].split(".");
+    let eventTime = eventDateTime.split(" ")[1].split(":");
     let eventTimestamp = new
         Date(
             eventDate[2],
